dbbf0d248c972219c640a1be0bd369d678836e79
meghna dhanvate <meghna.dhanvate>
2024-12-09 12:39:26 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="banner-homepage">
-      <h1>Hello World!</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, esse? Consequatur libero accusamus quam est blanditiis harum iste, quaerat repellat pariatur odit sit quia illum quas? Ab voluptate perspiciatis dignissimos!!</h1>
     </div>
   );
 }
